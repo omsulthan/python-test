74d94d66d8ddc171dbf78b7111f87b48ed6cb155
Sulthan <mromsulthan@gmail.com>
2024-07-01 22:17:37 +0700

    add api login

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_mysqldb import MySQL
+import jwt
+import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +12,8 @@ app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "push-db"
+# Secret key untuk encoding JWT
+app.config['SECRET_KEY'] = 'FahruJawir'
 
 mysql = MySQL(app)
 
@@ -17,8 +21,8 @@ mysql = MySQL(app)
 @app.route("/")
 def getAll():
     try:
-        nama = request.args.get('nama')
-        rating = request.args.get('rating')
+        nama = request.args.get("nama")
+        rating = request.args.get("rating")
         # Koneksi Ke Database
         conn = mysql.connection.cursor()
         # Menjalankan query berdasarkan parameter yang ada
@@ -70,6 +74,40 @@ def getDataById(id):
         if not result:
             return jsonify({"status": 404, "message": "Data not found"}), 404
         return jsonify(result)
+    except Exception as e:
+        print("Error:", str(e))
+        return jsonify({"status": 500, "message": "Internal server error"}), 500
+
+
+@app.route("/login", methods=["POST"])
+def postLogin():
+    try:
+        requestBody = request.get_json()
+        username = requestBody.get("username")
+        password = requestBody.get("password")
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM user WHERE username = %s", (username,))
+        data = cursor.fetchone()
+        column_names = [desc[0] for desc in cursor.description]
+        cursor.close()
+
+        if not data:
+            return jsonify({"status": 404, "message": "Data not found"}), 404
+
+        result = dict(zip(column_names, data))
+
+        # Memeriksa password dan mengembalikan hanya level, name, dan username
+        if result['password'] == password:
+            token = jwt.encode({
+                'username': result['username'],
+                'name': result['name'],
+                'level': result['level'],
+                'exp': datetime.datetime.now() + datetime.timedelta(hours=1)
+            }, app.config['SECRET_KEY'], algorithm="HS256")
+
+            return jsonify({"token": token, "status": 201, "message": "Login Berhasil"}), 201
+        else:
+            return jsonify({"status": 401, "message": "Password salah"}), 401
     except Exception as e:
         print("Error:", str(e))
         return jsonify({"status": 500, "message": "Internal server error"}), 500
